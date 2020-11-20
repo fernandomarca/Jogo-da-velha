5d737c72280077c1acf32526d454f5983d7b4e6e
fernandomarca <fernandomarca@hotmail.com>
2020-11-19 21:39:48 -0300

    finish

---
+++ interface.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let squares = document.querySelectorAll('.square');
+
+  squares.forEach((square) => {
+    square.addEventListener('click', handleClick);
+  })
+});
+
+function handleClick(event) {
+  // console.log(event.target);
+  let square = event.target;
+  let position = square.id;
+
+  if (handleMove(position)) {
+
+    setTimeout(() => {
+      alert('o jogo acabou');
+    }, 10);
+  }
+
+  updateSquare(position);
+}
+
+function reset() {
+  // board = ["", "", "", "", "", "", "", "", ""];
+  playerTime = 0;
+  gameOver = false;
+  updateSquaresReset();
+}
+
+function updateSquaresReset() {
+  let squares = document.querySelectorAll('.square');
+
+  squares.forEach((square) => {
+    let position = square.id;
+    let symbol = board[position];
+    console.log(symbol)
+    if (symbol != '') {
+      square.innerHTML = `<div></div>`;
+    }
+  })
+}
+
+function updateSquare(position) {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class='${symbol}'></div>`;
+}
